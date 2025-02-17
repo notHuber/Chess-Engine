3447efa2621b115f7f718a83501b74d2454a9956
freezato <congelataa@gmail.com>
2025-02-17 13:23:24 +0100

    chessboard

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Chess chess = new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        chess.chessBoard();
+
+        System.out.println();
+    }
+}
